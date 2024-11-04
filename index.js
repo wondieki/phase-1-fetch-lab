7458a5c3d0698fc6f979ee5b727543c60f856f95
wondieki <wkerubo28@gmail.com>
2024-11-04 10:46:08 +0300

    Done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(books => renderBooks(books));
 }
 
 function renderBooks(books) {
@@ -11,7 +14,6 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
